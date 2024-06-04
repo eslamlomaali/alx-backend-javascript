afcd2beda957a62ceb4b1c09265bac24cdcc856d
eslamlomaali <eslam@gmail.com>
2024-06-04 15:09:25 +0000

    done

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
-export default function updateStudentGradeByCity(comb, city, newGrades) {
-  return comb
-    .filter((element) => element.location === city)
-    .map((p) => {
-      const gs = newGrades.filter((element) => element.studentId === p.id);
-      const g = gs.length ? gs[0].g : 'N/A';
-      return {
-        ...p,
-        g,
-      };
-    });
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city).map((student) => {
+    const newS = student;
+    newS.grade = 'N/A';
+    for (const grade of newGrades) {
+      if (newS.id === grade.studentId) {
+        newS.grade = grade.grade;
+      }
+    }
+    return newS;
+  });
 }
